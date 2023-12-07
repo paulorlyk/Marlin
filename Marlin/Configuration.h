e47e4ac03ddb5e7f5df6dbb3fa56b446841cb15b
Paul Orlyk <paul.orlyk@gmail.com>
2023-12-07 21:29:19 +0200

    Update e-steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1226,7 +1226,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 403 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 415 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
