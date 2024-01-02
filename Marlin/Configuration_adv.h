cb12b44983975bbf4d409584b0b0bfb9c55b0739
Paul Orlyk <paul.orlyk@gmail.com>
2024-01-02 11:31:42 +0200

    Disable M73

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1654,7 +1654,7 @@
 #endif
 
 // Add 'M73' to set print job progress, overrides Marlin's built-in estimate
-#define SET_PROGRESS_MANUALLY
+//#define SET_PROGRESS_MANUALLY
 #if ENABLED(SET_PROGRESS_MANUALLY)
   #define SET_PROGRESS_PERCENT            // Add 'P' parameter to set percentage done
   #define SET_REMAINING_TIME              // Add 'R' parameter to set remaining time
